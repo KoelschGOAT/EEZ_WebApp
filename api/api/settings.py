91456a118b529493f9bbddfc6e4367f6bf301c0b
Luca Düpmann <luca.duepmann@gmail.com>
2022-09-13 12:12:26 +0200

    Removed unused GraphQL from Django Project

--- api/api/settings.py
+++ api/api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "corsheaders",
-    "graphene_django"
+    
 ]
 
 MIDDLEWARE = [
@@ -129,18 +129,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://192.168.3.23:3000", "http://localhost:5173",
-                        "http://192.168.178.41:3000", "http://192.168.178.21:3000", "http://172.16.81.73:3000"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:4173", "http://192.168.3.23:3000", "http://localhost:5173",
+                        "http://192.168.178.41:3000", "http://192.168.178.21:3000", "http://172.16.81.73:3000","http://localhost:4173"]
 X_FRAME_OPTIONS = 'ALLOWALL'
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:4173",
     "http://172.16.81.73:3000",
     "http://192.168.178.21:3000",
     "http://192.168.3.23:3000"
 ]
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:5173', "http://192.168.3.23:3000", "http://127.0.0.1:3000",
+    'http://localhost:4173', "http://192.168.3.23:3000", "http://127.0.0.1:3000",
 ]
 GRAPHENE = {
     "SCHEMA": "api.scheme.schema"
