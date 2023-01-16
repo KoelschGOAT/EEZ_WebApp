fdd0e12bf853bd0ecdaabc6eb6b81099456b40dd
Luca Düpmann <luca.duepmann@gmail.com>
2023-01-16 14:45:47 +0100

    edited CORS

--- api/api/settings.py
+++ api/api/settings.py
@@ -130,8 +130,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS=True
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173", "http://192.168.3.23:3000", "http://localhost:5173",
-                        "http://192.168.178.41:3000", "http://192.168.178.21:3000", "http://172.16.81.73:3000","http://localhost:4173"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173","http://192.168.178.155:5173""http://192.168.178.3:5173"]
 X_FRAME_OPTIONS = 'ALLOWALL'
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
@@ -139,9 +138,10 @@ CSRF_TRUSTED_ORIGINS = [
     "http://172.16.81.73:3000",
     "http://192.168.178.21:3000",
     "http://192.168.3.23:3000"
+    "http://192.168.178.155:5173""http://192.168.178.3:5173"
 ]
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:4173', "http://192.168.3.23:3000", "http://127.0.0.1:3000",
+    'http://localhost:4173', "http://192.168.3.23:3000", "http://127.0.0.1:3000","http://192.168.178.155:5173""http://192.168.178.3:5173"
 ]
 GRAPHENE = {
     "SCHEMA": "api.scheme.schema"
